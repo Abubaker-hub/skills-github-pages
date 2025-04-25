a090641a6b24c973ce86ec3b0aa2ce4ff36969ab
Abubaker-hub <abubakersaddique691@gmail.com>
2025-04-25 05:10:01 -0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,136 @@
+
+# husband_name = input("Enter your name: ")
+# wife_name = input("Enter your wife name: ")
+# husband_age = int(input("Enter your age: "))
+# wife_age = int(input("Enter your wife age: "))
+
+# if husband_age >= 19 and wife_age >= 15:
+#     print("You are a lovely/romantic couple.")
+# elif husband_age >= 19 and wife_age >= 20:
+#     print("You are a very cute/lovely/romantic couple.")
+# else:
+#     print("You are a cute old couple.")
+
+
+# for i in range(1, 101, 1):
+#     print(i, end=" ")
+#     if(i==50):
+#         break
+#     else:
+#         print("Mississippi")
+
+# print("Thank You")
+
+# num = [4,2,5,3,6,1,2,1,3,2,8,9,7]
+# print(num)
+# num.sort()
+# print(num)
+# num.sort(reverse=True)
+# print(num)
+# num.reverse()
+# print(num)
+
+# l = [11, 45, 1, 2, 4, 6]
+# print(l)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l.reverse()
+# print(l)
+# print(l.index(1))
+# print(l.count(2))
+"""m = l.copy()
+m[0] = 0
+print(m)"""
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+user_input = int(input("Enter Celsius Degrees: "))
+converted_celsius = celsius_to_fahrenheit(user_input)
+print(converted_celsius)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
