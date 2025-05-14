01c55a053f5851197fcca4df777350d496aaae3c
Abubaker-hub <abubakersaddique691@gmail.com>
2025-05-14 06:52:29 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -68,6 +68,30 @@ print(check_presence(2, s5))
 
 
 
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = gcd(num1, num2)
+print(f"The GCD of {num1} and {num2} is {result}")
+
+
+
+def gcd_iterative(a, b):
+    while b  != 0:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = gcd_iterative(num1, num2)
+print(f"The GCD of {num1} and {num2} is {result}")
 
 
 
